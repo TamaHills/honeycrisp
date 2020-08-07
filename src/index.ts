86d3fb1af099294fbef5ff1a6755d2decfcc7e08
matthew masters <matt.d.m@hotmail.com>
2020-08-07 04:30:08 -0400

    🔧 refactoring index.ts to prepare for mpd server

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,38 @@
 import { app, BrowserWindow } from 'electron';
 import { config } from 'dotenv' 
 import Axios from 'axios';
+
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 config()
 
-const createWindow = async () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+
+const createWindow = () => {  
+  return new BrowserWindow({
     height: 800,
     width: 1200,
     backgroundColor: '#3b4252',
     webPreferences: {
+      nodeIntegration: true,
       plugins: true
     }
   });
+};
+
+const initHoneyCrisp = async () => {
+  const mainWindow = createWindow()
 
   // load honeycrisp musickit token
   let res = await Axios.get(process.env.TOKEN_SERVER_URL)
 
   // and load the index.html of the app.
   mainWindow.loadURL(`${MAIN_WINDOW_WEBPACK_ENTRY}?token=${res.data.token}`);
-};
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', initHoneyCrisp);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
